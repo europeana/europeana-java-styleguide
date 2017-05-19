36127096b1b77581701a1e6cf84697074c4e2269
stzanakis <simon.tzanakis@europeana.eu>
2017-05-19 11:00:23 +0200

    Update project to handle updates on specific commits or branches rather than plain develop

---
+++ update_and_install.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+#Add patternlab remote
+git remote add --no-tags patternlab git@github.com:europeana/Europeana-Patternlab.git 2>/dev/null
+git fetch patternlab
+echo "Fetch completed"
+rm -rf source src target
+#Checkout branch for example patternlab/develop or replace with a specific commit SHA
+git checkout --progress patternlab/develop source/_patterns source/sass source/js/modules source/images
+if [ $? != 0 ]; then
+  echo "Branch or commit does not exist"
+  exit 1
+fi
+echo "Checked out patterlab source"
+mvn clean install
+
+exit 0
